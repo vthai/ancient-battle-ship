ef76236ea1748f0cbd39be2ddd624d6ba68c9e1a
vthai <veng.thai@gmail.com>
2014-03-25 22:46:56 +0700

    Initial commit for ancient battle ship, at this very late of the night 22:47...

---
+++ src/main/java/org/vthai/game/ancientbattleship/App.java
@@ -0,0 +1,13 @@
+package org.vthai.game.ancientbattleship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
